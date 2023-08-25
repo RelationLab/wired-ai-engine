f6f29fafddbdefe22e4a9baf570cd4a142d7c43d
chenjiaju <ccc-ju@outlook.com>
2023-08-25 21:40:06 +0800

    feat:修改Milvus单词拼写问题

--- base/__init__.py
+++ base/__init__.py
@@ -2,14 +2,14 @@ import os
 import openai
 import redis
 
-MILVUS_HOST = os.environ.setdefault('MLIVUS_HOST', '127.0.0.1')
-MILVUS_PORT = os.environ.setdefault('MLIVUS_PORT', '19530')
-MILVUS_USER = os.environ.setdefault('MLIVUS_USERNAME', 'wired-milvus')
-MILVUS_PASS = os.environ.setdefault('MLIVUS_PASSWORD', 'eqS9TjC7YbaviH9VdKTv')
+MILVUS_HOST = os.environ.setdefault('MILVUS_HOST', '127.0.0.1')
+MILVUS_PORT = os.environ.setdefault('MILVUS_PORT', '19530')
+MILVUS_USER = os.environ.setdefault('MILVUS_USERNAME', 'wired-milvus')
+MILVUS_PASS = os.environ.setdefault('MILVUS_PASSWORD', 'eqS9TjC7YbaviH9VdKTv')
 REDIS_HOST = os.environ.setdefault('REDIS_HOST', '10.5.0.2')
 REDIS_PORT = os.environ.setdefault('REDIS_PORT', '6379')
 REDIS_PASSWORD = os.environ.setdefault('REDIS_PASSWORD', '123456')
-openai.api_key = os.environ.setdefault('OPEN_AI_KEY', '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')
+openai.api_key = os.environ.setdefault('OPEN_AI_KEY', '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')
 OPENAI_EMBED_ENGINE = 'text-embedding-ada-002'
 OPENAI_API_BASE = 'https://www.googlex.vip/v1'
 openai.api_base = OPENAI_API_BASE
