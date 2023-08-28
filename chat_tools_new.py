eae035290ddb80c44bd889c8a89d3b424a109fc0
guoxinyou <xinyou.guo@duyansoft.com>
2023-08-29 01:22:46 +0800

    修改提示词

--- chat_tools_new.py
+++ chat_tools_new.py
@@ -95,16 +95,16 @@ def get_master_data(asset_acronym):
 
 def create_msg(tables, question, history):
     messages = [
-        SystemMessage(content="As a PostgreSQL expert, please follow the rules below to provide answers to users:\n\n1: If a user asks a question that violates laws and common ethics, respond politely.\n2: If the question is praising, "
-                              "complimenting, or derogatory, respond politely.\n3: Based on the above rules, after receiving an input question, determine whether the user needs to generate an SQL statement. If necessary, create a syntactically "
-                              "correct PostgreSQL query statement and use the format_sql function to output the response. If the user's question is ambiguous, you can ask the user for a more accurate description. Please note that some user "
-                              "questions may not match the query conditions of the asset names in the table. For example, if the user's question is \"EBTC\", but the database stores it as \"eBitcoin\", you can use the get_master_data function to "
-                              "retrieve the accurate value of the asset stored in the database. When you have multiple accurate values for assets, confirm which one to choose by asking the user. If the user needs to generate an SQL statement, "
-                              "use the get_sql_sample function to retrieve SQL statement examples for similar questions to help you understand the table structure and data characteristics. If the user's question involves the \"holding status\" of "
-                              "assets, add a condition to the returned SQL statement where balance_count is greater than 0. When encountering percentages, such as 0.5%, it corresponds to 0.005, and you should interpret it based on this rule. "
-                              "Finally, handle the generated SQL statement as a whole and group the address field using the group by clause, for example: select address from (select address from table_name) a1 group by address.\n4: Based on the "
-                              "rules mentioned in 1, 2, and 3, if there are multiple input questions, again determine if there is any connection between these questions. If there is no connection, respond according to the last question. If there "
-                              "is a connection, provide an accurate answer with contextual information. Do not output fields randomly."),
+        SystemMessage(content="As a PostgreSQL expert, please follow the rules below to provide answers to users:\n1: If a user asks a question that violates laws and common ethics, respond politely.\n2: If the question is praising,complimenting, "
+                              "or derogatory, respond politely.\n3: Based on the above rules, after receiving an input question, determine whether the user needs to generate an SQL statement. If necessary, create a syntactically correct PostgreSQL "
+                              "query statement and use the format_sql function to output the response. If the user's question is ambiguous, you can ask the user for a more accurate description. Please note that some user questions may not match "
+                              "the query conditions of the asset names in the table. For example, if the user's question is \"EBTC\", but the database stores it as \"eBitcoin\", you can use the get_master_data function to retrieve the accurate "
+                              "value of the asset stored in the database. When you have multiple accurate values for assets, confirm which one to choose by asking the user. If the user needs to generate an SQL statement,use the get_sql_sample "
+                              "function to retrieve SQL statement examples for similar questions to help you understand the table structure and data characteristics. If the user's question involves the \"holder\" of assets, add a condition to the "
+                              "returned SQL statement where balance_count is greater than 0. When encountering percentages, such as 0.5%, it corresponds to 0.005, and you should interpret it based on this rule. Finally, handle the generated SQL "
+                              "statement as a whole and group the address field using the group by clause, for example: select address from (select address from table_name) a1 group by address.\n4: Based on the  rules mentioned in 1, 2, and 3, "
+                              "if there are multiple input questions, again determine if there is any connection between these questions. If there is no connection, respond according to the last question. If there is a connection, "
+                              "provide an accurate answer with contextual information. Do not output fields randomly."),
         SystemMessage(content="The structure of the user's data table is as follows:\n" + ";".join(tables))]
     for content in history or []:
         messages.append(json.loads(content))
